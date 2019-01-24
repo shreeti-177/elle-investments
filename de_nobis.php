fb4d59d0f00d5f4595be84baf1f87d660c0adacf
luca <luca@192.168.1.105>
2019-01-24 12:45:05 -0500

    initial

---
+++ de_nobis.php
@@ -0,0 +1,105 @@
+<div id="main-content">
+    <?php include('crumbs.html'); ?> 
+    <style>
+        #aboutuspage {
+            color: #b09b4c;
+        }
+    </style>
+
+   <article>
+        
+        <h1 class="notranslate LcustomTranslation" id="investing_philosophy">Investing Philosophy</h1>
+
+        <p class="LcustomTranslation" id="D2">Elle Investments is a small family office specializing in asset management. The investment fund operated by Elle utilizes both quantitative and value investing strategies to seek out excellent high-return opportunities around the globe, with a particular emphasis on stocks that trade on the US markets. While a good part of our research focuses on small- and mid-cap equities in the biotech space, we are always on the lookout for favorable risk/reward investments across all sectors of the economy. Our trading strategies seek to take advantage of temporary mispricings that arise from overreactions to earnings misses, FDA decisions/clinical trial data readouts, macro news, etc., with the aim of generating returns that are independent of macro market movements. 
+        </p>
+
+        <h1 class="notranslate LcustomTranslation" id="about">About</h1>
+
+        <p class="LcustomTranslation" id="D2">About.    
+        </p>
+        
+        <h1 class="notranslate LcustomTranslation" id="published_research">Published Research</h1>
+
+        <p class="LcustomTranslation" id="D2">From time to time we publish our research on the investment platform <a href="https://seekingalpha.com/">Seeking Alpha</a>. Click <a href="https://seekingalpha.com/author/elle-investments#regular_articles">here</a> to be taken to our landing page on Seeking Alpha. The links to some of our articles are also listed below.    
+        </p>
+        
+        <p>
+        Symbol: IRIX</br>            
+        Date Published: October 2, 2018</br>  
+        <a href="https://seekingalpha.com/article/4209402-iridex-corporation-pretty-safe-penny-stock-ever-thing">Iridex Corporation: A Pretty Safe Penny Stock, If There Ever Was Such A Thing</a></br>
+        Investment Thesis: IRIX offers much less risk than what is usually found among stocks of such small market cap size. Prior to the drop from the recent public offering (which derisked the balance sheet), two consecutive earnings reports showed very strong growth from their Cyclo G6 laser platform, making it clear that they are on their way to disrupting the global glaucoma treatment market.
+        </p>
+        
+        <p>
+        Symbol: AMSC</br>            
+        Date Published: July 11, 2018</br>  
+        <a href="https://seekingalpha.com/article/4186516-american-superconductor-now">American Superconductor: Where To Now?</a></br>
+        Investment Thesis: The market was clearly not happy with the court settlement, given that after more than five years of litigation, AMSC was only able to recoup $58M in damages, even though losses in annual revenue stemming from IP theft at the hands of Chinese wind giant Sinovel totaled more than $500M. However, while news regarding the wind segment is pushing down the stock price, the company's other operating segment is set to begin commercialization of a technology that could revamp the electric grid nationwide, allowing neighborhoods that lose power from a storm or terrorist attack to regain it much faster than legacy technology allows.</br>   
+        </p>  
+        
+        <p>
+        Symbol: TUSK</br>            
+        Date Published: April 22, 2018</br>  
+        <a href="https://seekingalpha.com/article/4164623-mammoth-energy-services-buy-dip-recent-blackout-puerto-rico">Mammoth Energy Services: Buy The Dip From The Recent Blackout In Puerto Rico</a></br>
+        Investment Thesis: Puerto Rican officials were justified in voicing strong criticism over the TUSK subcontractor that caused back-to-back blackouts across the island due to negligent electrical work. But the 15% hit to the market cap seems like an overreaction, given that the subcontractor has already been fired, and prior to the dip, TUSK's stock price had been on a tear, driven largely from their quality work rebuilding Puerto Rico's electrical grid, which had garnered them several contract extensions.
+        </p>
+        
+        <h1 class="notranslate LcustomTranslation" id="internships">Internships</h1>
+
+        <p class="LcustomTranslation" id="D2">The analyst and programming internships are remote and are available year-round.
+        </p>
+        
+        <p>The <b>analyst</b> intern will assist in evaluating companies and making investment decisions. After being assigned a group of stocks, each intern will be responsible for putting together presentations explaining company segments, key products, important movements in the stock price, pertinent balance sheet information, earnings estimates, etc. and coming up with an investment recommendation.
+        </p>
+        
+        <p> The analyst research will be presented and discussed during Skype calls which usually take place Monday, Wednesday, and Friday around 12:00pm EST. Occasionally the days or hours may change. It is understandable if you need to skip a call from time to time to study for an exam, attend a job interview, etc. We just ask that you let us know in advance so that the calls can be scheduled to include maximum participation. Please only apply if you can commit at least 20 hours each week. As the internship runs year-round, you can always apply in the future if you think your current schedule will not allow substantial participation in both keeping up with your stock assignments and attending the Skype calls.    
+        </p>
+ 
+        <p>The <b>programming</b> intern will assist in the implementation and development of our database systems and software for strategy analysis, data acquisition, trading execution, and reporting.
+        </p>
+        
+        <p>Please click <a href="doc.Elle Quiz Analyst and Programmer Questionnaire 8E.pdf">here</a> to download the internship questionnaire.
+        </p>
+        
+        <h1 class="notranslate LcustomTranslation" id="contact">Contact</h1>
+
+        <p class="LcustomTranslation" id="D2">Please contact us at your convenience using the email address below.    
+        </p>
+        
+        <p>
+        <a href="mailto:ellel@live.com">ellel@live.com</a>
+        </p>
+
+            <footer id="colophon" class="clearfix notranslate"> 
+                <!-- goes inside <footer id="colophon">  -->
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<div class="container">
+    <aside class="widget">
+        <ul>
+            <li> <a href="index.php?page=about-us">Investing Philosophy</a> </li>
+            <li> <a href="index.php?page=terms_it">About</a> </li>
+            <li> <a href="index.php?page=privatezza">Published Research</a> </li>
+            <li> <a href="index.php?page=privatezza">Internships</a> </li>
+            <li> <a href="index.php?page=privatezza">Contact</a> </li>
+        </ul> 
+    </aside>
+
+    <!--This is for text in the middle of the footer-->
+    <aside class="widget">
+        <strong></strong><br/>
+        <div itemprop="address" itemscrop="" itemtype="http://schema.org/PostalAddress">
+            <span itemprop="telephone"></span><br/>
+            <br/>
+            <br/>
+        </div>
+    </aside>
+
+    <aside class="widget">
+        email address: <a href="mailto:ellel@live.com">ellel@live.com</a><br/>
+        <br/>
+        © 2018 Elle Investments. All Rights Reserved.<br/>
+        Site by <a href="www.renderterch.com" target="0">RenderTech, Inc.</a>
+    </aside>
+</div>
+</div> <!-- #main-content -->
---
+++ header.php
@@ -0,0 +1,42 @@
+<<div class="notranslate">
+    <div id="stars">
+        <!-- creates a hidden div on top of the page where the stars are displayed
+             right clicking on the div will lead to a new page -->
+        <div oncontextmenu="javascript:window.location.href = '../privato/index.php';" 
+             style="opacity: 0; width: 50px; height:50px; left: 23px; cursor:default; 
+             background-color:red; position: absolute;"></div>
+        <h1>Elle Investments </h1>
+    </div>
+    <h4> Asset Management</h4>    
+
+
+
+
+    <div class="middle-bar"></div>
+</div> 
+            </div>
+            <div id="main-container" class="clear"> 
+                <div id="tableDiv">
+                    <div id="main-content">
+    <ul class="crumbs">
+    <li><a href="#investing_philosophy"
+           class="LcustomTranslation wasF2 notranslate"
+           id="aboutuspage">Investing Philosophy</a></li>
+    <li><a href="#about"
+           id="professionalspage">About</a></li>
+    <li><a href="#published_research" 
+           id="contacuspage">Published Research</a></li>
+    <li><a href="#internships" 
+           id="contacuspage">Internships</a></li>       
+    <li><a href="#contact" 
+           id="contacuspage">Contact</a></li>
+</ul> 
+    <style>
+        #aboutuspage {
+            color: #b09b4c;
+        }
+    </style>
+
+
+    <div class="middle-bar"></div>
+</div>
