411293885108bfa7f004c7e302d22ef61a2c5c57
luca <luca@192.168.1.105>
2019-03-29 14:52:47 -0400

    Research + nocollapse

--- LoginF/login.php
+++ LoginF/login.php
@@ -68,6 +68,38 @@
 
 <html>
     <body>
+        <div>
+    <table style="margin-bottom: 0px;">
+        <tbody>
+            <tr>
+                <td id="left-header" width="80%">
+                    <div id="stars">
+                        <!-- creates a hidden div on top of the page where the stars are displayed
+                             right clicking on the div will lead to a new page -->
+                        <div oncontextmenu="javascript:window.location.href = '../privato/index.php';" 
+                             style="opacity: 0; width: 50px; height:50px; left: 23px; cursor:default; 
+                             background-color:red; position: absolute;"></div>
+
+                        <h1 style="display:inline-block">Elle Investments </h1>
+                    </div>
+                    <h4> Asset Management</h4>
+                </td>
+                <td id="right-header" width="20%" style=" text-align: center">
+                    <div style="display: inline-block">
+                       
+                        <img src = "../images/ELLE LOGO-L_100x56.png" width="100" height="56px" alt="Elle Investments logo" >
+                    </div>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+
+    <?php include('crumbs.html'); ?> 
+    <!--
+    <div class="middle-bar"></div> -->
+</div> 
+
         <form action="login.php" method="POST">
                 User: </br>
                 <input type="text" name="username" value="<?php 
@@ -93,5 +125,6 @@
                 ?>>
                 <label for="remember">Remember me</label>
             </form>
+        <p> <br>This page is for internal use or authorized clients only.</p>
     </body>
 </html>
