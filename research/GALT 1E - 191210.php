d745965fdbf2e3354827bb643aba91ffc4dfd669
luca <luca@192.168.0.194>
2020-03-24 13:29:30 -0400

    Research Updates 200324

---
+++ research/GALT 1E - 191210.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+    <body>
+
+        <?php
+        /* show banner if cookies is not accepted */
+        if (!isset($_COOKIE['accept-cookies'])) {
+
+            include ('cookie-notice-banner/cookie-notice-banner.html'); 
+           /* JQuery so the banner slides down and css for cookie notice banner */
+            echo '<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.0/jquery.min.js"></script>'
+            . '<script src="../cookie-notice-banner/cookie-notice-banner.js"></script>';
+        }
+        ?>
+
+        <div id="supra-header"></div>
+        <div id="page">
+            <div id="header"> 
+                <?php include('../mainFolder/header.php'); ?> 
+            </div>
+             <div id="main-container" class="clear"> 
+                <br>
+                <div id="tableDiv">
+                    <?php include('GALT 1E - 191210.html'); ?>
+
+                </div>
+
+        </div>
+    </body>
+</html>
